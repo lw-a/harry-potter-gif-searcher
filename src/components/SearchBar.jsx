1609c6d0cd84f9f3d81e1c9e1bd900cbfdb45665
Adam Hemming <hemming.dev@gmail.com>
2022-09-08 16:06:36 +0900

    make all searches harry potter related

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,10 +3,10 @@ import React from 'react';
 const SearchBar = (props) => {
   const handleChange = (event) => {
     const { fetchGiphy } = props;
-    fetchGiphy(event.currentTarget.value);
+    fetchGiphy(`${event.currentTarget.value} harry potter`);
   };
   return (
-    <input className="form-search form-control" type="text" placeholder="Search for a gif" onChange={handleChange} />
+    <input className="form-search form-control" type="text" placeholder="Search for a Harry Potter gif" onChange={handleChange} />
   );
 };
 
